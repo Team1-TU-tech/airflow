7439c668989128cd2069b441f1126703b6ddc7b8
hahahellooo <hahahello777@gmail.com>
2025-01-03 14:05:30 +0900

    need to fix java

--- install.sh
+++ install.sh
@@ -9,6 +9,11 @@ apt update && apt install -y \
     wget curl unzip libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1 \
     libxrender1 libxi6 libxtst6 libx11-xcb1 x11-utils git
 
+# JAVA
+#wget https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u372-b07/OpenJDK8U-jdk_x64_linux_hotspot_8u372b07.tar.gz
+#tar -xzf OpenJDK8U-jdk_x64_linux_hotspot_8u372b07.tar.gz -C /usr/local/
+#mv /usr/local/jdk8u372-b07 /usr/local/java
+
 # Google Chrome 설치
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 apt install -y ./google-chrome-stable_current_amd64.deb
